eb36f945c04ba0a4a833d7e79259b85b8d6995a0
JuankaEsMa <joancarles.escobar@gmail.com>
2023-12-17 18:17:59 +0100

    provider HTTP

--- Hamaca/src/app/services/chollo.service.ts
+++ Hamaca/src/app/services/chollo.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Chollo } from '../models/chollo.model';
+import { Filters } from '../models/filters.model';
 
 @Injectable({
   providedIn: 'root'
@@ -15,13 +16,19 @@ export class CholloService {
   getCholloById(id:string):Observable<Chollo>{
     return this.httpClient.get("https://proyecto-final-backend-production-c6e8.up.railway.app/chollo/"+id)
   }
-  getAllChollosFiltered(filters:Array<string>):Observable<any>{
-    let url:string =  "https://proyecto-final-backend-production-c6e8.up.railway.app/chollo";
-
-    for (let i = 0; i < filters.length; i++) {
-      filters[i];
-    }
-
-    return this.httpClient.get("");
+  getAllChollosFiltered(filters:Filters):Observable<any>{
+    let url:string =  "https://proyecto-final-backend-production-c6e8.up.railway.app/chollo?localidad="
+    +filters.localidad+"&tematica="+filters.tematica+"&pais="+filters.pais+"&dataInicio="+filters.dataInicio+"&dataFinal="
+    +filters.dataFinal+"&precioMin="+filters.precioMin+"&precioMax="+filters.precioMax+"&page="+filters.page+"&size="+filters.size;
+    return this.httpClient.get(url);
+  }
+  deleteCholloById(id:string){
+    return this.httpClient.delete("https://proyecto-final-backend-production-c6e8.up.railway.app/chollo/"+id)
+  }
+  addChollo(chollo:Chollo){
+    return this.httpClient.post("https://proyecto-final-backend-production-c6e8.up.railway.app/chollo/", chollo);
+  }
+  addTematicaInChollo(id:string, idTematica:string){
+    return this.httpClient.post("https://proyecto-final-backend-production-c6e8.up.railway.app/chollo/"+id, {id:idTematica});
   }
 }
