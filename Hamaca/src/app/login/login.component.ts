1289cd430386393220fb8d669a81bfa846320aef
JuankaEsMa <joancarles.escobar@gmail.com>
2023-12-20 22:56:29 +0100

    my user profile

--- Hamaca/src/app/login/login.component.ts
+++ Hamaca/src/app/login/login.component.ts
@@ -37,7 +37,6 @@ export class LoginComponent implements AfterViewInit {
             event.preventDefault()
             event.stopPropagation()
           }
-
           form.classList.add('was-validated')
         }, false)
       })
